a3daa4ed38fe40d7fc38015acc3e0450d95127be
Rokas <rookas.rudzenskas@gmail.com>
2021-04-30 10:24:47 +0300

    Rokas

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -12,32 +12,31 @@ const LineGraph = () => {
     // it is going to be able to draw it in the table
 // that casesType does the exact same thing as adding the data[cases], emaning it goes and grabs the cases, and not anything different
     // x is going to be the date, and y is going to be cases for that day
-    const buildChartData = (data, casesType='cases') => {
-        const chartData = []
+
+    const buildChartData = (data, casesType = 'cases') => {
+        let chartData = [];
         // the object which is currently empty
         let lastDataPoint;
         // every single entry, then we loop each time is going to be the date on the x coord
-        data[casesType].cases.forEach(date => {
-            if(lastDataPoint) {
-                const newDataPoint = {
+        for (let date in data.cases) {
+            if (lastDataPoint) {
+                let newDataPoint = {
                     x: date,
                     // y because we are going to show it for the cases, and it goes to the data object, to the cases, to the cases date, and
-                    // - the lastDataPoint. We do so, because we want to get the differece between two dates, how much it decreased or increased
-                    // the lastDataPoint, is the last cases number
-                    // basically y becomes the difference between last day and the current day
-                    // data, the cases, because we are going to show the chart for the data and not for deaths or something like that
-                    y: data[casesType][date] - lastDataPoint // the difference between the last day, and the next day. shows all the new cases
-                }
+//                 // - the lastDataPoint. We do so, because we want to get the differece between two dates, how much it decreased or increased
+//                 // the lastDataPoint, is the last cases number
+//                 // basically y becomes the difference between last day and the current day
+//                 // data, the cases, because we are going to show the chart for the data and not for deaths or something like that
+                    y: data[casesType][date] - lastDataPoint,  // the difference between the last day, and the next day. shows all the new cases
+                };
                 // we push the formed x and y to the newly formed chart data array
-                chartData.push(newDataPoint); // 🔝
+                chartData.push(newDataPoint);
             }
             // we just update the lasst dataa point, to the next day,
-            lastDataPoint = data[casesType][date]; // sets the last date to the next day
-        })
-        return chartData
-
-    }
-
+            lastDataPoint = data[casesType][date];  // sets the last date to the next day
+        }
+        return chartData;
+    };
 
     useEffect(() => {
         fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
@@ -65,3 +64,31 @@ const LineGraph = () => {
 }
 
 export default LineGraph;
+
+
+
+// const buildChartData = (data, casesType="cases") => {
+//     const chartData = []
+//     // the object which is currently empty
+//     let lastDataPoint;
+//     // every single entry, then we loop each time is going to be the date on the x coord
+//     data[casesType].forEach((date) => {
+//         if(lastDataPoint) {
+//             const newDataPoint = {
+//                 x: date,
+//                 // y because we are going to show it for the cases, and it goes to the data object, to the cases, to the cases date, and
+//                 // - the lastDataPoint. We do so, because we want to get the differece between two dates, how much it decreased or increased
+//                 // the lastDataPoint, is the last cases number
+//                 // basically y becomes the difference between last day and the current day
+//                 // data, the cases, because we are going to show the chart for the data and not for deaths or something like that
+//                 y: data[casesType][date] - lastDataPoint // the difference between the last day, and the next day. shows all the new cases
+//             };
+//             // we push the formed x and y to the newly formed chart data array
+//             chartData.push(newDataPoint); // 🔝
+//         }
+//         // we just update the lasst dataa point, to the next day,
+//         lastDataPoint = data[casesType][date]; // sets the last date to the next day
+//     });
+//     return chartData;
+//
+// };
