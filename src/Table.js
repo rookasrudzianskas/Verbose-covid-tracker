ff38ba22f790683596b43513bf94cc4497adc4aa
Rokas <rookas.rudzenskas@gmail.com>
2021-04-30 08:32:23 +0300

    Rokas

--- src/Table.js
+++ src/Table.js
@@ -4,11 +4,12 @@ const Table = ({ countries }) => {
     return (
         <div className="table">
             {/* we go per all the countries, and map each country to the differnet table row*/}
-            {countries.map(country => (
+            {/* it is going to split up to the country and the cases, and take for each iteration the differnt values and display*/}
+            {countries.map(({country, cases}) => (
                 // tr>td*2
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td>{country}</td>
+                    <td><strong>cases</strong></td>
                 </tr>
             ))}
         </div>
