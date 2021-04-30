5267810a7af027b6f3c10611515d03173c4bc82d
Rokas <rookas.rudzenskas@gmail.com>
2021-04-30 06:43:19 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import {FormControl, MenuItem, Select} from "@material-ui/core";
+import {useState} from "react";
 
 function App() {
+    const [countries, setCountries] = useState(['USA']);
+
   return (
     <div className="app">
-      <h1>Hello world! 🚀</h1>
+        <div className="app__header">
+      <h1>COVID-21 TRACKER</h1>
+        <FormControl className="app__dropdown">
+            <Select variant="outlined" value="abc">
+                {/* loop thought all the countries and show the a drop down of possible options*/}
+
+                {
+                    countries.map(country => (
+                        <MenuItem value={country}>{country}</MenuItem>
+                    ))
+                }
+
+
+                {/*<MenuItem value="Rokas">Rokas</MenuItem>*/}
+                {/*<MenuItem value="Rokas">Option two</MenuItem>*/}
+                {/*<MenuItem value="Rokas">Option 3</MenuItem>*/}
+                {/*<MenuItem value="Rokas">Option 4</MenuItem>*/}
+            </Select>
+        </FormControl>
+        </div>
+
+    {/*    Header   */}
+    {/*    Title and select input dropdown field    */}
+
+    {/*    info boxes*/}
+    {/*    info boxes*/}
+    {/*    info boxes*/}
+
+    {/*    table    */}
+    {/*    Graph    */}
+
+    {/*    map*/}
     </div>
   );
 }
