0b7b2d3345738c63bf2c14723f2728d4beaf90ec
Rokas <rookas.rudzenskas@gmail.com>
2021-04-30 15:46:39 +0300

    Rokas

--- src/util.js
+++ src/util.js
@@ -42,7 +42,22 @@ export const showDataOnMap = (data, casesType='cases') =>
             }
         >
         <Popup>
-            <h1>I am poppup</h1>
+            <div className="info-container">
+                <div
+                    className="info-flag"
+                    style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+                ></div>
+                <div className="info-name">{country.country}</div>
+                <div className="info-confirmed">
+                    Cases: {numeral(country.cases).format("0,0")}
+                </div>
+                <div className="info-recovered">
+                    Recovered: {numeral(country.recovered).format("0,0")}
+                </div>
+                <div className="info-deaths">
+                    Deaths: {numeral(country.deaths).format("0,0")}
+                </div>
+            </div>
         </Popup>
         </Circle>
     ));
