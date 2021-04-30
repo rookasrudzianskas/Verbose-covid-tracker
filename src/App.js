5733a76b12333b1d95c476343b438f4cbd518356
Rokas <rookas.rudzenskas@gmail.com>
2021-04-30 07:11:06 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import {FormControl, MenuItem, Select} from "@material-ui/core";
 import {useEffect, useState} from "react";
 
 function App() {
-    const [countries, setCountries] = useState(['USA']);
+    const [countries, setCountries] = useState([]);
+    const [country, setCountry] = useState('worldwide')
 
     // https://disease.sh/v3/covid-19/countries
 
     useEffect(() => {
         // Thee code inside will run once the component loads, and not again after
+        // async -> send a request, wait for it and then do something
 
-    }, [countries])
+        const getCountriesData = async () => {
+            // getting the json object from this link
+            await fetch("https://disease.sh/v3/covid-19/countries")
+                // from the response, we just take away the json object
+                .then((response) => response.json())
+                // mapping throug all the countries in the json object, to make the array of countries, which consist of objects
+                // which has name and value
+                .then((data) => {
+                    const countries = data.map((country) => (
+                        {
+                            name: country.country, // United States, India
+                            value: country.countryInfo.iso2 // UK, USA, FR
+                        }));
+                    setCountries(countries);
+                });
+        };
+        getCountriesData();
+    }, [])
 
 
   return (
@@ -18,12 +37,12 @@ function App() {
         <div className="app__header">
       <h1>COVID-21 TRACKER</h1>
         <FormControl className="app__dropdown">
-            <Select variant="outlined" value="abc">
+            <Select variant="outlined" value={country}>
                 {/* loop thought all the countries and show the a drop down of possible options*/}
-
+                <MenuItem value="worldwide">Worldwide</MenuItem>
                 {
                     countries.map(country => (
-                        <MenuItem value={country}>{country}</MenuItem>
+                        <MenuItem value={country.value}>{country.name}</MenuItem>
                     ))
                 }
 
